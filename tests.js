6855743a6eaa0f907b1c3af53125093891a9e2dd
Sakari Mursu <sakari@mursu.dev>
2019-05-31 00:17:14 +0300

    Improve tests error messages

--- tests.js
+++ tests.js
@@ -6,18 +6,23 @@ const fs = require("fs");
 const doggo = require("./doggonlang.js");
 
 const hint1 = fs.readFileSync("hint1.dog", "utf8");
-assert(doggo.interpret(hint1) == 11, "Hint #1 result should be 11");
+const result1 = doggo.interpret(hint1);
+assert(result1 == 11, `Hint #1 result should be 11, was ${result1}`);
 
 const hint2 = fs.readFileSync("hint2.dog", "utf8");
-assert(doggo.interpret(hint2) == 15, "Hint #2 result should be 15");
+const result2 = doggo.interpret(hint2);
+assert(result2 == 15, `Hint #2 result should be 15, was ${result2}`);
 
 const hint3 = fs.readFileSync("hint3.dog", "utf8");
-assert(doggo.interpret(hint3) == 105, "Hint #3 result should be 105");
+const result3 = doggo.interpret(hint3);
+assert(result3 == 105, `Hint #3 result should be 105, was ${result3}`);
 
 const hint4 = fs.readFileSync("hint4.dog", "utf8");
-assert(doggo.interpret(hint4) == 19, "Hint #4 result should be 19");
+const result4 = doggo.interpret(hint4);
+assert(result4 == 19, `Hint #4 result should be 19, was ${result4}`);
 
 const task = fs.readFileSync("task.dog", "utf8");
-assert(doggo.interpret(task) == 64185, "Task result should be 64185");
+const resultTask = doggo.interpret(task);
+assert(resultTask == 64185, `Task result should be 64185, was ${resultTask}`);
 
 console.log("All tests ok!");
